058d7dcbe541e0cc35412233ba685a93422defac
Roshan R <roshan.atl@gmail.com>
2017-05-23 16:14:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -172,8 +172,8 @@ def run_inference_on_image(image_data):
         human_string = label_lines[node_id]
         score = predictions[0][node_id]
         print('%s (score = %.5f)' % (human_string, score))
-	data[human_string] = score
-
+	details= ("{\"%s\" : \"%.5f\"}" % (human_string, score))
+        data.append(details)
   # map to the friendly names and return the tuples
   return data
  
